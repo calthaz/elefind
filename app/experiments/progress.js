a2cae7b17d400347f294f338a584086aa826d156
Caltha <calthayzh@gmail.com>
2017-02-04 16:05:57 +0800

    add new files

---
+++ app/experiments/progress.js
@@ -0,0 +1,39 @@
+$(function(){
+	var canvas = document.getElementById('progress');
+    var ctx = canvas.getContext('2d');
+	ctx.lastX = 0;
+	ctx.lastY = 0; 
+	ctx.lastAction = 0; 
+
+
+});
+
+function uniKeyCode(event) {
+    var key = event.which || event.keyCode;
+    //document.getElementById("demo2").innerHTML = "The Unicode KEY code is: " + key;
+    if(key == 84){
+
+    }else if(key == 77){
+
+    }else if(key == 83){
+
+    }else if(key >= 49 && key <= 57){
+
+    }else if (key == 68){
+    	
+    }
+    //threas t 84
+    //typicalmap m 77
+    //start s 83
+    //nubmer 1 49
+    //number 2 
+    //3
+    //4
+    //5
+    //6
+    //7
+    //8
+    //9 57
+    //
+    //done d 68
+}
